b62ae83a079b53d3e1523a790bcbdd1b09ada17a
diegodramos08 <56616239+diegodramos08@users.noreply.github.com>
2020-07-02 22:27:19 -0600

    Add files via upload

--- ExamenPa2/Ex2/GcdFSM.v
+++ ExamenPa2/Ex2/GcdFSM.v
@@ -15,34 +15,114 @@ module GcdFSM(
 );
 
    reg [3:0] cs/*verilator public*/;
+   reg [3:0] ns;
 
    always @(posedge clk) begin
 
       case (cs)
-         2'd0: begin
-            rf_write_addr = 32'h1;
+         4'd0: begin
             rf_write_en = 1'b1;
+            rf_write_addr = 5'd0; //reg[0]
+            rf_write_data_sel = 1'b1; //write const_val
+            const_val = a; //reg[0] = a
+            ns = 4'd1;
+         end
+         4'd1: begin
+            rf_write_en = 1'b1; 
+            rf_write_addr = 5'd1; //reg[1]
+            rf_write_data_sel = 1'b1; //write const_val
+            const_val = b; //reg[1] = b
+            ns = 4'd2;
+         end
+         4'd2: begin
+            rf_write_en = 1'b1;
+            rf_write_addr = 5'd2;
             rf_write_data_sel = 1'b1;
-            const_val = a;
+            const_val = 0;
+            alu_sel = 1'b0; // ALU op2 -> read_data_2
+            alu_oper = 2'd1; // ALU oper -
+            rf_read_addr1 = 5'd0; // reg[0]
+            rf_read_addr2 = 5'd1; // reg[1]
+            //reg[0] - reg[1]
+            ns = 4'd3;
          end
-         2'd1: begin
-            
+         4'd3: begin
+            if(is_zero_result) //reg[0] - reg[1] == 0 -> a == b
+               ns = 4'hf; // FINAL
+            else  begin
+               rf_write_en = 1'b1; 
+               rf_write_addr = 5'd2; //write reg[2]
+               rf_write_data_sel = 1'b0; //write Alu result
+               rf_read_addr1 = 5'd1; //read1 reg[1] (b)
+               rf_read_addr2 = 5'd0; //read2 reg[0] (a)
+               alu_sel = 1'b0; // ALU op2 -> read_data_2
+               alu_oper = 2'd3; // ALU oper <
+               // b < a
+               ns = 4'd4;
+            end
          end
-         2'd2: begin
-            rf_write_data_sel = 1'b0;
+         4'd4: begin
+            rf_read_addr1 = 5'd2; // reg[2]
+            const_val = 0; 
+            alu_sel = 1'b1; // reg[2](resultado de b < a) + 0
+            alu_oper = 2'd0; // ALU oper + 
+            // reg[2] + 0 
+            // terminaria siendo 1 + 0  o  0 + 0
+            ns = 4'd6;
+         end
+         4'd6: begin
+            if(is_zero_result) begin 
+               //basicamente 1 + 0 == 0  o  0 + 0 == 0
+               rf_write_en = 1'b1;
+               rf_write_addr = 5'd2; // write reg[2]
+               rf_read_addr1 = 5'd1; // read1 reg[1]
+               rf_read_addr2 = 5'd0; // read2 reg[0]
+               rf_write_data_sel = 1'b0; // write ALU Result
+               alu_oper = 2'd1; // ALU oper -
+               alu_sel = 1'b0; // ALU op2 -> read2 
+               // reg[2] = reg[1](b) - reg[0](a) 
+               ns = 4'd7;
+            end
+            else begin
+               rf_write_en = 1'b1; 
+               rf_write_addr = 5'd2; // write reg[2]
+               rf_read_addr1 = 5'd0; // read1 reg[0]
+               rf_read_addr2 = 5'd1; // read2 reg[1]
+               rf_write_data_sel = 1'b0; // wrtie ALU Result
+               alu_oper = 2'b1; // ALU oper -
+               alu_sel = 1'b0; // ALU op2 read2
+               // reg[2] = reg[0](a) - reg[1](b)
+               ns = 4'd8;
+            end
+         end
+         4'd7: begin
             rf_write_en = 1'b1;
+            rf_write_addr = 5'd1;
+            rf_write_data_sel = 1'b0;
+            rf_read_addr1 = 5'd2;
+            const_val = 0;
+            alu_sel = 1'b1;
+            alu_oper = 2'd0;
+            ns = 4'd2;
          end
-         2'd3: begin
-
+         4'd8: begin
+            rf_write_en = 1'b1;
+            rf_write_addr = 5'd0;
+            rf_write_data_sel = 1'b0;
+            rf_read_addr1 = 5'd2;
+            const_val = 1;
+            alu_sel = 1'b1;
+            alu_oper = 2'd2;
+            ns = 4'd2;
+         end
+         default: begin
+            ns = 4'hf;
          end
       endcase
-
-      case (cs)
-         2'd0: ns = 2'd1;
-      endcase
+      $display("cs: %d", cs);
 
       if(rst)
-         cs <= 2'd0;
+         cs <= 4'd0;
       else
          cs <= ns;
    end
